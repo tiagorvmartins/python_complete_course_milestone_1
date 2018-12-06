81351abfba1d2e7c6c3d593e10eb5b197d0bc74d
Tiago Martins <tiagorvmartins@gmail.com>
2018-12-06 23:11:04 +0000

    Final version of milestone 1

--- app_jonas.py
+++ app_jonas.py
@@ -7,7 +7,7 @@ def menu():
         if user_input == 'a':
             add_movie()
         elif user_input == 'l':
-            show_movies()
+            show_movies(movies)
         elif user_input == 'f':
             find_movie()
         else:
@@ -15,10 +15,11 @@ def menu():
 
         user_input = input("\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, 'q' to quit: ")
 
+
 def add_movie():
     name = input('Enter the movie name: ')
     director = input('Enter the movie director: ')
-    year = int(input('Enter the movie release year: '))
+    year = input('Enter the movie release year: ')
 
     movies.append({
         'name': name,
@@ -26,14 +27,35 @@ def add_movie():
         'year': year
     })
 
-def show_movies():
-    for movie in movies:
+
+def show_movies(movies_list):
+    for movie in movies_list:
         show_movie_details(movie)
+
 
 def show_movie_details(movie):
     print(f"Name: {movie['name']}")
     print(f"Director: {movie['director']}")
     print(f"Year: {movie['year']}")
+
+
+def find_movie():
+    find_by = input("What property of the movie are you looking for? ")
+    looking_for = input("What are you searching for? ")
+
+    found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
+
+    show_movies(found_movies)
+
+
+def find_by_attribute(items, expected, finder):
+    found = []
+    for i in items:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
+
 
 
 menu()
